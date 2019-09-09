4ddaaf7a88d34b3ef36b8a3e26b3c672e000cc76
Bjørnar Trondsen <bjornar@trondsen.org>
2019-09-09 13:09:00 +0200

    file  upload

--- src/main/java/com/example/springbootfileupload/SpringBootFileUploadApplication.java
+++ src/main/java/com/example/springbootfileupload/SpringBootFileUploadApplication.java
@@ -2,11 +2,22 @@ package com.example.springbootfileupload;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SpringBootFileUploadApplication {
+	private static ApplicationContext applicationContext;
+
 
 	public static void main(String[] args) {
+		applicationContext = new AnnotationConfigApplicationContext(SpringBootFileUploadApplication.class);
+
+		for (String beanName : applicationContext.getBeanDefinitionNames()) {
+			System.out.println("After ComponentScan: " + beanName);
+		}
+
 		SpringApplication.run(SpringBootFileUploadApplication.class, args);
 	}
 
